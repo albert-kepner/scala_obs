5834222d17502ea13077eab3eaa797b3cf189feb
asus-alkepner <asus-alkepner@ASUS-ALKEPNER>
2017-05-27 02:51:25 -0400

    First Pass Interaction and InteractionTest

--- observatory/src/main/scala/observatory/Interaction.scala
+++ observatory/src/main/scala/observatory/Interaction.scala
@@ -1,46 +1,113 @@
 package observatory
 
-import com.sksamuel.scrimage.{Image, Pixel}
+import com.sksamuel.scrimage.{ Image, Pixel }
 
 /**
-  * 3rd milestone: interactive visualization
-  */
+ * 3rd milestone: interactive visualization
+ */
 object Interaction {
 
   /**
-    * @param zoom Zoom level
-    * @param x X coordinate
-    * @param y Y coordinate
-    * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
-    */
+   * @param zoom Zoom level
+   * @param x X coordinate
+   * @param y Y coordinate
+   * @return The latitude and longitude of the top-left corner of the tile, as per http://wiki.openstreetmap.org/wiki/Slippy_map_tilenames
+   */
   def tileLocation(zoom: Int, x: Int, y: Int): Location = {
-    ???
+    val point: LatLonPoint = Tile(x, y, zoom.toShort).toLatLon
+    Location(point.lat, point.lon)
   }
 
+  def tileSubtileLocations(zoom: Int, x: Int, y: Int, addZoomLevel: Int): Unit = {
+    val twoPower: Int = 1 << addZoomLevel
+
+    println(s"twoPower = $twoPower")
+
+    val baseLoc = tileLocation(zoom, x, y)
+    println(s"baseLoc = $baseLoc")
+    val xPlus = tileLocation(zoom, x + 1, y)
+    println(s"xPlus = $xPlus")
+    val yPlus = tileLocation(zoom, x, y + 1)
+    println(s"yPlus = $yPlus")
+
+    for (
+      col <- (0 until twoPower);
+      row <- (0 until twoPower)
+    ) {
+      val subtileLoc = tileLocation(zoom + addZoomLevel, x * twoPower + col, y * twoPower + row)
+
+      if (col <= 2 && row <= 2) {
+        // detail output
+        println(s"col = $col, row = $row, subtileLoc = $subtileLoc")
+      }
+    }
+  }
   /**
-    * @param temperatures Known temperatures
-    * @param colors Color scale
-    * @param zoom Zoom level
-    * @param x X coordinate
-    * @param y Y coordinate
-    * @return A 256×256 image showing the contents of the tile defined by `x`, `y` and `zooms`
-    */
+   * @param temperatures Known temperatures
+   * @param colors Color scale
+   * @param zoom Zoom level
+   * @param x X coordinate
+   * @param y Y coordinate
+   * @return A 256×256 image showing the contents of the tile defined by `x`, `y` and `zooms`
+   */
   def tile(temperatures: Iterable[(Location, Double)], colors: Iterable[(Double, Color)], zoom: Int, x: Int, y: Int): Image = {
-    ???
+
+    val debug: Boolean = false;
+
+    val imageWidth: Int = 256
+    val imageHeight: Int = 256
+
+    val pixLen: Integer = imageWidth * imageHeight
+    val pixels = new Array[Pixel](pixLen)
+
+    val twoPower: Int = 256
+    val addZoomLevel: Int = 8
+    var index: Int = 0
+
+    for (
+      col <- (0 until twoPower);
+      row <- (0 until twoPower)
+    ) {
+      val pixelLoc: Location = tileLocation(zoom + addZoomLevel, x * twoPower + col, y * twoPower + row)
+      val pixelTemp: Double = Visualization.predictTemperature(temperatures, pixelLoc)
+      val pixelColor: Color = Visualization.interpolateColor(colors, pixelTemp)
+      val pixel: Pixel = Pixel(pixelColor.red, pixelColor.green, pixelColor.blue, 255)
+      pixels(index) = pixel
+      index += 1
+
+    }
+    val image: Image = Image(imageWidth, imageHeight, pixels)
+    if (debug || true) {
+      println(s"image: $image")
+    }
+    image
   }
 
   /**
-    * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
-    * @param yearlyData Sequence of (year, data), where `data` is some data associated with
-    *                   `year`. The type of `data` can be anything.
-    * @param generateImage Function that generates an image given a year, a zoom level, the x and
-    *                      y coordinates of the tile and the data to build the image from
-    */
+   * Generates all the tiles for zoom levels 0 to 3 (included), for all the given years.
+   * @param yearlyData Sequence of (year, data), where `data` is some data associated with
+   *                   `year`. The type of `data` can be anything.
+   * @param generateImage Function that generates an image given a year, a zoom level, the x and
+   *                      y coordinates of the tile and the data to build the image from
+   */
   def generateTiles[Data](
     yearlyData: Iterable[(Int, Data)],
-    generateImage: (Int, Int, Int, Int, Data) => Unit
-  ): Unit = {
-    ???
+    generateImage: (Int, Int, Int, Int, Data) => Unit): Unit = {
+    yearlyData.foreach {
+      _ match {
+        case (year, data) => {
+          for(zoom <- (0 to 3) ) {
+            val twoPower: Int = 1<<zoom
+            for(row <- 0 until twoPower;
+              col <- 0 until twoPower) {
+              generateImage(year, zoom, col, row, data)
+            }
+          }
+          
+        }
+      }
+
+    }
   }
 
 }
--- observatory/src/test/scala/observatory/InteractionTest.scala
+++ observatory/src/test/scala/observatory/InteractionTest.scala
@@ -10,4 +10,60 @@ import scala.collection.concurrent.TrieMap
 @RunWith(classOf[JUnitRunner])
 class InteractionTest extends FunSuite with Checkers {
 
+  //  test("tile at 1,1,1") {
+  //    val loc: Location = Interaction.tileLocation(1, 1, 1)
+  //    println(s"location = $loc")
+  //  }
+  //  test("tile at 2,2,2") {
+  //    val loc: Location = Interaction.tileLocation(2, 2, 2)
+  //    println(s"location = $loc")
+  //  }
+  //  test("tile at zoom 3, 4,4") {
+  //    val loc: Location = Interaction.tileLocation(3, 4, 4)
+  //    println(s"location = $loc")
+  //  }
+  //
+  test("tile at zoom 4, 8,8") {
+    val loc: Location = Interaction.tileLocation(4, 8, 8)
+    println(s"location = $loc")
+  }
+
+  test("tile at zoom 5,16,16") {
+    val loc: Location = Interaction.tileLocation(5, 16, 16)
+    println(s"location = $loc")
+  }
+  test("tile at zoom 5,17,17") {
+    val loc: Location = Interaction.tileLocation(5, 17, 17)
+    println(s"location = $loc")
+  }
+
+  test("tileSubtileLocations 1") {
+    Interaction.tileSubtileLocations(5, 16, 16, 1)
+    Interaction.tileSubtileLocations(5, 16, 16, 2)
+    Interaction.tileSubtileLocations(5, 16, 16, 3)
+    Interaction.tileSubtileLocations(5, 16, 16, 4)
+    Interaction.tileSubtileLocations(5, 16, 16, 8)
+  }
+
+  test("tile Image 1st test") {
+    val l1 = Location(0, -9)
+    val l2 = Location(0, -7)
+    val l3 = Location(5, -1)
+
+    val t1 = (l1, 12.0)
+    val t2 = (l2, 22.0)
+    val t3 = (l3, 30.0)
+    val temps = List(t1, t2, t3)
+
+    val red = Color(255, 0, 0)
+    val blue = Color(0, 0, 255)
+    val green = Color(0, 255, 0)
+    val p1: (Double, Color) = (10, red)
+    val p2: (Double, Color) = (20, blue)
+    val p3: (Double, Color) = (30, green)
+    val colors = List(p1, p2, p3)
+
+    val image = Interaction.tile(temps, colors, 5, 16, 16)
+  }
+
 }
